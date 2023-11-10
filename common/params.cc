e7cf3379f0e7104eff797d778f047f85831afd92
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-11-10 13:21:42 -0700

    Lock doors when in drive for Toyota/Lexus
    Added toggle to automatically lock the doors when in drive for Toyota/Lexus vehicles.
    
    Credit goes to AlexandreSato!
    
    https: //github.com/AlexandreSato/openpilot
    Co-Authored-By: Alexandre Nobuharu Sato <66435071+alexandresato@users.noreply.github.com>

--- common/params.cc
+++ common/params.cc
@@ -250,6 +250,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
     {"LeadInfo", PERSISTENT},
+    {"LockDoors", PERSISTENT},
     {"LongitudinalTune", PERSISTENT},
     {"MapOpen", PERSISTENT},
     {"MuteDM", PERSISTENT},
