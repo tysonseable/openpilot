7f3dbed55eab062e860bd0868c77ea3e3ca26d07
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-11-10 13:21:42 -0700

    Rotating steering wheel in onroad UI
    Added toggle to rotate the steering wheel in the top right corner of the onroad UI.

--- common/params.cc
+++ common/params.cc
@@ -291,6 +291,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"RoadEdgesWidth", PERSISTENT},
     {"RoadName", PERSISTENT},
     {"RoadNameUI", PERSISTENT},
+    {"RotatingWheel", PERSISTENT},
     {"ScreenBrightness", PERSISTENT},
     {"ShowCPU", PERSISTENT},
     {"ShowFPS", PERSISTENT},
