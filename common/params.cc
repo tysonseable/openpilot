07c6ba8acd226a8a73ec194a9256215c0f33bdf7
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-11-10 13:21:42 -0700

    Model switcher
    Added model selector to swap between several different models on the fly.

--- common/params.cc
+++ common/params.cc
@@ -254,6 +254,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LongitudinalTune", PERSISTENT},
     {"LowerVolt", PERSISTENT},
     {"MapOpen", PERSISTENT},
+    {"Model", PERSISTENT},
+    {"ModelList", PERSISTENT},
     {"MuteDM", PERSISTENT},
     {"MuteDoor", PERSISTENT},
     {"MuteOverheated", PERSISTENT},
