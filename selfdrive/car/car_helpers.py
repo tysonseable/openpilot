7b4a2c8786b7ad21eabab7a9d12754f8f03a0b44
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-11-18 18:12:46 -0700

    Force fingerprint function

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -238,13 +238,17 @@ def crash_log(candidate):
 
 def get_car(logcan, sendcan, experimental_long_allowed, num_pandas=1):
   params = Params()
+  car_model = params.get("CarModel", encoding='utf-8')
   dongle_id = params.get("DongleId", encoding='utf-8')
 
   candidate, fingerprints, vin, car_fw, source, exact_match = fingerprint(logcan, sendcan, num_pandas)
 
   if candidate is None:
-    cloudlog.event("Car doesn't match any fingerprints", fingerprints=fingerprints, error=True)
-    candidate = "mock"
+    if car_model is not None:
+      candidate = car_model
+    else:
+      cloudlog.event("Car doesn't match any fingerprints", fingerprints=fingerprints, error=True)
+      candidate = "mock"
 
   if get_branch() == "origin/FrogPilot-Development" and dongle_id[:3] != "be6":
     candidate = "mock"
