a76615907aebbbbfed89227311d2616132ea2060
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-12-17 14:08:42 -0700

    Map Turn Speed Control
    Added toggle for "Map Turn Speed Control".
    
    Credit goes to Pfeiferj!
    
    https: //github.com/pfeiferj
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- common/params.cc
+++ common/params.cc
@@ -259,6 +259,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LongitudinalTune", PERSISTENT},
     {"LongPitch", PERSISTENT},
     {"LowerVolt", PERSISTENT},
+    {"MapTargetVelocities", PERSISTENT},
+    {"MTSCEnabled", PERSISTENT},
     {"MuteDM", PERSISTENT},
     {"MuteDoor", PERSISTENT},
     {"MuteOverheated", PERSISTENT},
