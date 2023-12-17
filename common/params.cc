39792945016e26f14d7f20536c3126772ee96d53
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-12-17 14:08:42 -0700

    Driver camera view when in reverse
    Added toggle to show the driver camera when in the reverse gear.

--- common/params.cc
+++ common/params.cc
@@ -244,6 +244,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CustomTheme", PERSISTENT},
     {"DeviceShutdown", PERSISTENT},
     {"DisableOnroadUploads", PERSISTENT},
+    {"DriverCamera", PERSISTENT},
     {"FrogPilotTogglesUpdated", PERSISTENT},
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
