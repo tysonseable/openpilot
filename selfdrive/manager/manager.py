b65e6a9b8d6dba06a8a2b1d3f5d416285d948ea4
jakethesnake420 <fastermaster12@gmail.com>
2024-01-06 18:49:30 -0600

    fix record back toggle

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -48,6 +48,7 @@ def manager_init() -> None:
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
     ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
+    ("RecordBack", "1"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
