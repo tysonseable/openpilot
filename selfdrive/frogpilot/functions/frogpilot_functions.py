c30707dbc14bff5fb95cd4a374f60c9aabb669b2
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-02-27 16:34:47 -0700

    Model switcher
    Added model selector to swap between several different models on the fly.

--- selfdrive/frogpilot/functions/frogpilot_functions.py
+++ selfdrive/frogpilot/functions/frogpilot_functions.py
@@ -8,6 +8,8 @@ from openpilot.system.hardware import HARDWARE
 params = Params()
 params_memory = Params("/dev/shm/params")
 
+DEFAULT_MODEL = "los-angeles"
+
 CRUISING_SPEED = 5  # Roughly the speed cars go when not touching the gas while in drive
 THRESHOLD = 5       # Time threshold (0.25s)
 
