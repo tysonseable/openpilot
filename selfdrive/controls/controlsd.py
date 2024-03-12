438b16f56c8c6b44fa5957db1c6005d65add35a8
Anthony <maiorania@gmail.com>
2024-03-12 08:42:38 -0700

    test

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -279,15 +279,15 @@ class Controls:
       #self.events.add(EventName.radarFault)
     if not self.sm.valid["pandaStates"]:
       self.events.add(EventName.usbError)
-    elif not self.sm.all_alive_and_valid() or self.can_rcv_error:
-      self.events.add(EventName.commIssue)
-      if not self.logged_comm_issue:
-        invalid = [s for s, valid in self.sm.valid.items() if not valid]
-        not_alive = [s for s, alive in self.sm.alive.items() if not alive]
-        cloudlog.event("commIssue", invalid=invalid, not_alive=not_alive, can_error=self.can_rcv_error, error=True)
-        self.logged_comm_issue = True
-    else:
-      self.logged_comm_issue = False
+    #elif not self.sm.all_alive_and_valid() or self.can_rcv_error:
+    #  #self.events.add(EventName.commIssue)
+    #if not self.logged_comm_issue:
+    #    invalid = [s for s, valid in self.sm.valid.items() if not valid]
+    #    not_alive = [s for s, alive in self.sm.alive.items() if not alive]
+    #    cloudlog.event("commIssue", invalid=invalid, not_alive=not_alive, can_error=self.can_rcv_error, error=True)
+    #    self.logged_comm_issue = True
+    #else:
+    #  self.logged_comm_issue = False
 
     if not self.sm['liveParameters'].valid:
       self.events.add(EventName.vehicleModelInvalid)
